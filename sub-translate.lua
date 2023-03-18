d5e911bd7b5aee7fb7ceae5b2426695a5a25b86e
meme <meme@M.com>
2023-03-18 23:21:58 +0800

    v0.12

--- sub-translate.lua
+++ sub-translate.lua
@@ -2,6 +2,9 @@ local mp = require 'mp'
 local utils = require 'mp.utils'
 local json = require 'dkjson'
 
+local target_language = "zh-CN" -- Set your desired target language here
+local pre_fetch_delay = 2 -- Pre-fetch subtitles in advance (in seconds)
+
 local function urlencode(str)
     if str then
         str = string.gsub(str, "\n", "\r\n")
@@ -15,7 +18,6 @@ end
 
 local function translate(text, target_language)
     local url_request = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=" .. target_language .. "&dt=t&q=" .. urlencode(text)
-
     mp.msg.info("Request URL: " .. url_request)  -- Print the request URL
 
     local res, err = utils.subprocess({ args = { "curl", "-s", "-S", "-f", url_request } })
@@ -46,7 +48,7 @@ function get_sub()
     res['start'] = mp.get_property("sub-start")
     res['end'] = mp.get_property("sub-end")
     return res
-  end
+end
 
 local function escape_special_characters(str)
     local escaped_str = string.gsub(str, '([\\{}%[%]%(%)%\\])', '\\%1') -- Escape necessary characters
@@ -54,37 +56,51 @@ local function escape_special_characters(str)
     return escaped_str
 end
 
-local function display_translated_subtitle()
-    local target_language = "zh-CN" -- Set your desired target language here
-    local sub = get_sub()
-    if sub == nil then return; end
-    escaped_sub = escape_special_characters(sub['text'])
-    mp.msg.info('escaped_sub:' .. escaped_sub)
-    local translated_text = translate(escaped_sub, target_language)
-
-    mp.msg.info('sub:' .. sub['text'] .. 'start:' .. sub['start'] .. 'end:' .. sub['end'] )
-    time_duration = tonumber(sub['end'] - sub['start']) * 1000
+local function display_translated_subtitle(translated_text, time_start, time_end)
+    local time_duration = tonumber(time_end - time_start) * 1000
     if translated_text then
         mp.msg.info("translated_text: " .. translated_text) -- Print the translated text to the console
         translated_text = translated_text:gsub('"', '\\"')
         local command_string = string.format("show-text '${osd-ass-cc/0}{\\an5}${osd-ass-cc/1}%s' %i", translated_text, time_duration)
-
         mp.command(command_string)
-
     else
         mp.msg.error("Translation error")
     end
 end
 
 
+local last_sub_start = nil
+local last_translated_text = nil
 
-
-
-mp.observe_property("sub-text", "string", function(_, text)
-    if text and text ~= "" then
-        display_translated_subtitle()
-        --show_translated_sub(text)
-    else
-        mp.set_property("osd-ass-cc/0", "")
+local function check_and_display_subtitles()
+    local sub = get_sub()
+    if sub == nil or sub['start'] == last_sub_start then
+        return
     end
+
+    last_sub_start = sub['start']
+
+    local escaped_sub = escape_special_characters(sub['text'])
+    mp.msg.info('escaped_sub:' .. escaped_sub)
+    local translated_text = translate(escaped_sub, target_language)
+
+    if translated_text then
+        mp.msg.info("translated_text: " .. translated_text) -- Print the translated text to the console
+        last_translated_text = translated_text
+        display_translated_subtitle(translated_text, sub['start'], sub['end'])
+    else
+        mp.msg.error("Translation error")
+    end
+end
+
+local subtitle_check_timer = mp.add_periodic_timer(0.5, check_and_display_subtitles) -- Check every 0.5 seconds
+
+mp.register_event("file-loaded", function()
+    last_sub_start = nil
+    last_translated_text = nil
+    subtitle_check_timer:resume()
+end)
+
+mp.register_event("end-file", function()
+    subtitle_check_timer:kill()
 end)
